b539065f2adaffcb69eb99bb9948a88903b82e06
acsolabs <sofian.achbabi@gmail.com>
2026-03-19 10:41:48 +0100

    update : api change

--- index.js
+++ index.js
@@ -3,23 +3,26 @@
 let usersData = [];
 
 const fetchUsers = async () => {
-  await fetch("https://dummyjson.com/users?limit=24")
+  await fetch("https://randomuser.me/api/?results=24")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      usersData = data.users;
-      console.log(usersData);
+      console.log(data.results[0]);
+      usersData = data.results;
     });
 };
 
 const displayUsers = async () => {
   await fetchUsers();
-  console.log("test 1 ", usersData);
-  usersData.map((user) => {
-    document.body.innerHTML += `<div class="card">
-    <h3>${user.firstName}</h3>
-    </div>`;
-  });
-};
 
+  document.body.innerHTML = usersData
+    .map(
+      (user) =>
+        `
+       <div class="card">
+        <h3>${user.name.first} ${user.name}</h3>
+      </div>
+      `,
+    )
+    .join(" ");
+};
 displayUsers();
