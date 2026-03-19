cf4d6d74e869ac81d5425874f5279e31df2d97da
acsolabs <sofian.achbabi@gmail.com>
2026-03-19 11:27:26 +0100

    registered date added

--- index.js
+++ index.js
@@ -23,15 +23,21 @@ const displayUsers = async () => {
     return newDate;
   };
 
+  const dayCalc = (date) => {
+    let today = new Date();
+    let todayTimestamp = Date.parse(today);
+    let timestamp = Date.parse(date);
+    return Math.ceil((todayTimestamp - timestamp) / 8.64e7);
+  };
   document.body.innerHTML = usersData
     .map(
       (user) =>
         `
        <div class="card">
-       <img src="${user.picture.large}"><img>
+       <img src="${user.picture.large}" alt="photo de ${user.name.first}"><img>
        <h3>${user.name.first} ${user.name.last}</h3>
-        <p>${user.location.city}, ${dateParser(user.dob.date)}<p>
-        <em>membre depuis : ${user.registered.date}</em>
+      <p>${user.location.city}, ${dateParser(user.dob.date)}<p>
+        <em>membre depuis : ${dayCalc(user.registered.date)} jours</em>
       </div>
       `,
     )
